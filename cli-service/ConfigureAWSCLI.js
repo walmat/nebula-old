41670a609a247da4be26c6e0fd96ab13c8e65a58
John Koehn <kingkoehn@gmail.com>
2018-04-03 21:02:52 -0500

    Attempt to use aws configure
    May go down a route of setting local env variables instead.

--- cli-service/ConfigureAWSCLI.js
+++ cli-service/ConfigureAWSCLI.js
@@ -3,7 +3,7 @@ const exec = util.promisify(require( 'child_process' ).exec);
 
 module.exports  = async function configureAWSAccount(awsUserAccount) {
 	try {
-		cmd = await exec('aws help');
+		cmd = await exec(`aws configure\n\r${awsUserAccount.aws_access_key}\n\r${awsUserAccount.awsSecertAccessKey}\n\r${awsUserAccount.region}\n\r`);
 		console.log('STDOUT', cmd.stdout);
 		console.log(cmd.stderr);
 	} catch (err) {
