ce3dc42d9a7919ca263cffeb13b50c4d7121ff8a
John Koehn <kingkoehn@gmail.com>
2018-04-22 15:38:29 -0500

    creating proxy

--- aws-service/ec2SSH.js
+++ aws-service/ec2SSH.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 let password = 'test'
 let conn = new Client();
 
-let dns = 'ec2-35-174-111-139.compute-1.amazonaws.com';
+let dns = 'ec2-54-234-51-151.compute-1.amazonaws.com';
 let ipAddress = '35.174.111.139';
 
 function sleep(ms){
@@ -14,99 +14,7 @@ function sleep(ms){
 }
 
 async function ec2SSH(Instance, ec2, privateKey) {
-    // try {
-    //     // await ssh.connect({
-    //     //     host: dns,
-    //     //     username: 'ubuntu',
-    //     //     privateKey: 'test.pem'
-    //     //   });
-
-    //     //   await ssh.connect({
-    //     //     host: Instance.PublicDnsName,
-    //     //     username: 'root',
-    //     //     privateKey: privateKey
-    //     //   });
-
-    //     console.log('hey');
-    //     // await ssh.execCommand('echo hello', { cwd:'~' }).then(function(result) {
-    //     //     console.log('STDOUT: ' + result.stdout)
-    //     //     console.log('STDERR: ' + result.stderr)
-    //     // });
-
-    //     // await ssh.execCommand('mkdir test', { cwd:'~' }).then(function(result) {
-    //     //     console.log('STDOUT: ' + result.stdout)
-    //     //     console.log('STDERR: ' + result.stderr)
-    //     // });
-    //     // await ssh.exec('sudo passwd ubuntu', { cwd:'~', options: { pty: true }}).then(function(err, stream) {
-    //     //     if(err) {
-    //     //         throw err;
-    //     //     }
-    //     //     stream.on('close', function(code, signal) {
-    //     //         console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
-    //     //         return;
-    //     //     }).on('data', function(data) {
-    //     //         if (data.indexOf(':') >= data.length - 2) {
-    //     //         stream.write('test' + '\n');
-    //     //         }
-    //     //         else {
-    //     //             console.log('STDOUT: ' + data);
-    //     //         }
-    //     //     }).stderr.on('data', function(data) {
-    //     //         console.log('STDERR: ' + data);
-    //     //     });
-    //     // });
-
-    //     // await sleep(1000);
-
-    //     // ssh.execCommand('whoami', { cwd:'~' }).then(function(result) {
-    //     //     console.log('STDOUT: ' + result.stdout)
-    //     //     console.log('STDERR: ' + result.stderr)
-    //     // });
-
-    //     // await ssh.execCommand('wget https://transfer.sh/riJC3/nebula.sh', { cwd:'~' }).then(function(result) {
-    //     //     console.log('STDOUT: ' + result.stdout)
-    //     //     console.log('STDERR: ' + result.stderr)
-    //     // });
-
-    //     // await ssh.execCommand('chmod +x nebula.sh', { cwd:'~' }).then(function(result) {
-    //     //     console.log('STDOUT: ' + result.stdout)
-    //     //     console.log('STDERR: ' + result.stderr)
-    //     // });
-
-    //     await ssh.execCommand('sudo echo test', { cwd:'~' }).then(function(result) {
-    //         console.log('STDOUT: ' + result.stdout)
-    //         console.log('STDERR: ' + result.stderr)
-    //     });
-
-    //     await ssh.execCommand('sudo ./nebula.sh', { cwd: '~', options: { pty: true } }).then(function(result) {
-    //         console.log('STDOUT: ' + result.stdout)
-    //         console.log('STDERR: ' + result.stderr)
-    //     })
-
-
-    // } catch (err) {
-    //     console.log(err);
-    // }
-
-    conn.on('ready', function() {
-        console.log('Client :: ready');
-        conn.exec('sudo su', { pty: true }, function(err, stream) {
-            if (err) throw err;
-            stream.on('close', function(code, signal) {
-                console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
-                conn.end();
-            }).on('data', function(data) {
-                if (data.indexOf(':') >= data.length - 2) {
-                stream.write(password + '\n');
-                }
-                else {
-                    console.log('STDOUT: ' + data);
-                }
-            }).stderr.on('data', function(data) {
-                console.log('STDERR: ' + data);
-            });
-        });
-    }).connect({
+    conn.on('ready', createProxy).connect({
         host: dns,
         username: 'ubuntu',
         privateKey: fs.readFileSync('test.pem')
@@ -115,6 +23,32 @@ async function ec2SSH(Instance, ec2, privateKey) {
 }
 
 ec2SSH();
+
+function createProxy() {
+    console.log('Client :: ready');
+    conn.exec('sudo su', { pty: true }, function (err, stream) {
+        if (err) throw err;
+        stream.on('close', function () {
+            console.log('Stream :: close');
+            conn.end();
+        }).on('data', function (data) {
+            console.log('STDOUT: ' + data);
+
+            let dataString = data.toString().toUpperCase()
+            if (dataString.includes('Y/N')) {
+                stream.write('y\n');
+            } else if (dataString.includes('USERNAME')) {
+                stream.write('nebula\n');
+            } else if (dataString.includes('PASSWORD')) {
+                stream.write('nebula\n');
+            }
+        }).stderr.on('data', function (data) {
+            console.log('STDERR: ' + data);
+        });
+        stream.write('wget https://transfer.sh/riJC3/nebula.sh && chmod +x nebula.sh && ./nebula.sh\n');
+        //stream.end('echo bye');
+    });
+}
 
 // ip 35.173.177.45
 // public dns name 'ec2-35-173-177-45.compute-1.amazonaws.com'
