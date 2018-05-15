776914bfb47fa5caac5dec5d2289d8aa85601224
mwall <mwall@iastate.edu>
2018-05-14 21:14:24 -0500

    updated website to show current UI changes

--- backend/dynamoDBSetup.js
+++ backend/dynamoDBSetup.js
@@ -1,4 +1,4 @@
-// Run this to set up your local dynamodb tables necessary to develop the backend
+// Run this to set up your local dynamoDB tables necessary to develop the backend
 var AWS = require("aws-sdk");
 
 AWS.config.update({
@@ -10,25 +10,49 @@ AWS.config.update({
 
 var dynamodb = new AWS.DynamoDB();
 
-var params = {
-    TableName : "Profiles",
-    KeySchema: [       
+var profiles = {
+    TableName : "profiles",
+    KeySchema: [
         { AttributeName: "registrationKey", KeyType: "HASH"},  //Partition key
         { AttributeName: "profileName", KeyType: "RANGE" }  //Sort key
     ],
-    AttributeDefinitions: [       
+    AttributeDefinitions: [
         { AttributeName: "registrationKey", AttributeType: "S" },
         { AttributeName: "profileName", AttributeType: "S" }
     ],
-    ProvisionedThroughput: {       
-        ReadCapacityUnits: 5, 
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
         WriteCapacityUnits: 5
     }
 };
 
-dynamodb.createTable(params, function(err, data) {
+var users = {
+    TableName : "users",
+    KeySchema: [
+        { AttributeName: "discordURI", KeyType: "RANGE"},  //Partition key
+        { AttributeName: "registrationKey", KeyType: "HASH" }  //Sort key
+    ],
+    AttributeDefinitions: [
+        { AttributeName: "discordURI", AttributeType: "S" },
+        { AttributeName: "registrationKey", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
+        WriteCapacityUnits: 5
+    }
+};
+
+dynamodb.createTable(users, function(err, data) {
     if (err) {
-        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+        console.log("Error creating table users.", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table: Users.", JSON.stringify(data, null, 2));
+    }
+});
+
+dynamodb.createTable(profiles, function(err, data) {
+    if (err) {
+        console.error("Unable to create table profiles. Error JSON:", JSON.stringify(err, null, 2));
     } else {
         console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
     }
