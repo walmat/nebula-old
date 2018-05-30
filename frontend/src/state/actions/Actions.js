556f43ba7b7d252097422b32b7d0fe1587e46ff6
Srinivas Dhanwada <dhanwads@gmail.com>
2018-05-30 11:54:47 -0500

    Begin Adding Actions and Reducers
    This commit starts to add redux actions and reducers for the
    app, starting with the profiles components.

---
+++ frontend/src/state/actions/Actions.js
@@ -0,0 +1,8 @@
+/**
+ * This file is merely a shared import point for all actions.
+ */
+
+ import { editLocation, LOCATION_FIELDS } from './profiles/LocationActions';
+
+ export const editLocation = editLocation;
+ export const LOCATION_FIELDS = LOCATION_FIELDS;
---
+++ frontend/src/state/reducers/Reducers.js
@@ -0,0 +1,6 @@
+/**
+ * Container for all state reducers. Reducers are available in their specific
+ * files, this is just a shared import point.
+ */
+
+ 
