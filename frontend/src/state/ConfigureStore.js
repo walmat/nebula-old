b6cc3d33a5ecd97806143fd1efe1909fe6a5caf1
Srinivas Dhanwada <dhanwads@gmail.com>
2018-06-05 19:47:45 -0500

    Refactor Validation Middleware
    In preparation for adding middleware for complete form validation (on
    save), the attribute validation middleware has been consolidated into a
    single middleware. This reduces the middleware count and also allows us
    to clearly have an attribute AND form validation middleware without
    confusion.

--- frontend/src/state/ConfigureStore.js
+++ frontend/src/state/ConfigureStore.js
@@ -1,17 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import topLevelReducer, { initialState } from './Reducers';
-import paymentFormValidationMiddleware from './middleware/profiles/paymentFormValidationMiddleware';
-import locationFormValidationMiddleware from './middleware/profiles/locationFormValidationMiddleware';
-import { PROFILE_FIELDS } from './Actions';
+import profileAttributeValidationMiddleware from './middleware/profiles/profileAttributeValidationMiddleware';
 
 export default function configureStore() {
   return createStore(
     topLevelReducer,
     initialState,
     applyMiddleware(
-      paymentFormValidationMiddleware,
-      locationFormValidationMiddleware(PROFILE_FIELDS.EDIT_SHIPPING),
-      locationFormValidationMiddleware(PROFILE_FIELDS.EDIT_BILLING),
+      profileAttributeValidationMiddleware,
     ),
   );
 }
