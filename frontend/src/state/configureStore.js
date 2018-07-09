ab3c192164a73c81cbe204821841b90c984b7e9c
Srinivas Dhanwada <dhanwads@gmail.com>
2018-07-09 18:16:59 -0500

    Add DevTools to Electron
    This commit adds developer tools to electron, so debugging redux and
    react issues will be much easier!

--- frontend/src/state/configureStore.js
+++ frontend/src/state/configureStore.js
@@ -1,17 +1,19 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import topLevelReducer, { initialState } from './reducers';
 import profileAttributeValidationMiddleware from './middleware/profiles/profileAttributeValidationMiddleware';
 import profileFormValidationMiddleware from './middleware/profiles/profileFormValidationMiddleware';
 import profileApiMiddleware from './middleware/profiles/profileFormApiMiddleware';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default function configureStore() {
   return createStore(
     topLevelReducer,
     initialState,
-    applyMiddleware(
+    composeEnhancers(applyMiddleware(
       profileAttributeValidationMiddleware,
       profileFormValidationMiddleware,
       profileApiMiddleware,
-    ),
+    )),
   );
 }
