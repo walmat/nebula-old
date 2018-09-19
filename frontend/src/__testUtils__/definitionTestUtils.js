f81125687b5a9d85169bc4421185e5068ae3bae0
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2018-09-19 01:58:25 -0500

    Move Array Testing to Definition Utils
    This commit moves the tests from the profileList definitions to the definition test utils and generalizes the tests. The profileList tests are updated to use it and tests for the taskList tests.

--- frontend/src/__testUtils__/definitionTestUtils.js
+++ frontend/src/__testUtils__/definitionTestUtils.js
@@ -25,7 +25,7 @@ export const testKey = (keyName, validKey, invalidKey, spec, initialState, spy) 
   };
 
   const _testKey = (value, isValid) => {
-    it(`should succeed when ${isValid ? '' : 'in'}valid value "${value}" is passed to ${keyName}`, () => {
+    it(`should ${isValid ? 'succeed' : 'fail'} when ${isValid ? '' : 'in'}valid value "${value}" is passed to ${keyName}`, () => {
       const testState = {
         test: {
           ...initialState,
@@ -51,5 +51,57 @@ export const testKey = (keyName, validKey, invalidKey, spec, initialState, spy) 
     invalidKey.forEach(value => _testKey(value, false));
   } else {
     _testKey(invalidKey, false);
+  }
+};
+
+export const testArray = (validEntries, invalidEntries, spec, initialEntries, spy) => {
+  const specWrapper = {
+    test: spec,
+  };
+
+  const _testArray = (value, isValid) => {
+    it(`it should ${isValid ? 'succeed' : 'fail'} when a non-empty array containing ${isValid ? '' : 'in'}valid values is passed`, () => {
+      const testArr = initialEntries.splice(0);
+      if (Array.isArray(value)) {
+        value.forEach(v => testArr.push(v));
+      } else {
+        testArr.push(value);
+      }
+      const testState = {
+        test: testArr,
+      };
+      PropTypes.checkPropTypes(specWrapper, testState, 'test', `${isValid ? 'success' : 'failure'} check`);
+      if (isValid) {
+        expect(spy.consoleError).not.toHaveBeenCalled();
+      } else {
+        expect(spy.consoleError).toHaveBeenCalled();
+      }
+    });
+  };
+
+  it('should succeed when an empty array is passed', () => {
+    const testState = {
+      test: [],
+    };
+    PropTypes.checkPropTypes(specWrapper, testState, 'test', 'success check');
+    expect(spy.consoleError).not.toHaveBeenCalled();
+  });
+
+  it('should fail when an object is passed', () => {
+    const testState = {
+      test: { object: 'invalid' },
+    };
+    PropTypes.checkPropTypes(specWrapper, testState, 'test', 'fail check');
+    expect(spy.consoleError).toHaveBeenCalled();
+  });
+
+  // Test all valid entries at once
+  _testArray(validEntries, true);
+
+  // Test invalid entries one by one
+  if (Array.isArray(invalidEntries)) {
+    invalidEntries.forEach(value => _testArray(value, false));
+  } else {
+    _testArray(invalidEntries, false);
   }
 };
--- frontend/src/__tests__/utils/definitions/profiles/profileList.test.js
+++ frontend/src/__tests__/utils/definitions/profiles/profileList.test.js
@@ -2,44 +2,22 @@
 import PropTypes from 'prop-types';
 
 import pDefns, { initialProfileStates } from '../../../../utils/definitions/profileDefinitions';
-import { setupConsoleErrorSpy } from '../../../../__testUtils__/definitionTestUtils';
+import { setupConsoleErrorSpy, testArray } from '../../../../__testUtils__/definitionTestUtils';
 
 describe('profileList definitions', () => {
   const spy = setupConsoleErrorSpy();
 
-  const specWrapper = {
-    test: pDefns.profileList,
-  };
-
-  it('should succeed when an empty array is passed', () => {
-    const testState = {
-      test: [],
-    };
-    PropTypes.checkPropTypes(specWrapper, testState, 'test', 'success check');
-    expect(spy.consoleError).not.toHaveBeenCalled();
-  });
-
-  it('should succeed when a non-empty array of profiles is passed', () => {
-    const testState = {
-      test: [initialProfileStates.profile],
-    };
-    PropTypes.checkPropTypes(specWrapper, testState, 'test', 'success check');
-    expect(spy.consoleError).not.toHaveBeenCalled();
-  });
-
-  it('should fail when a non-empty array of invalid profiles is passed', () => {
-    const testState = {
-      test: [{ id: true }],
-    };
-    PropTypes.checkPropTypes(specWrapper, testState, 'test', 'fail check');
-    expect(spy.consoleError).toHaveBeenCalled();
-  });
-
-  it('should fail when an object is passed', () => {
-    const testState = {
-      test: { object: 'invalid' },
-    };
-    PropTypes.checkPropTypes(specWrapper, testState, 'test', 'fail check');
-    expect(spy.consoleError).toHaveBeenCalled();
-  });
+  testArray(
+    [
+      initialProfileStates.profile,
+      { id: 1, profileName: 'testing' },
+    ],
+    [
+      { id: true },
+      { id: 2, profileName: 42 },
+    ],
+    pDefns.profileList,
+    initialProfileStates.list,
+    spy,
+  );
 });
