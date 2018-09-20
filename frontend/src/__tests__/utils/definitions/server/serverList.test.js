939006df50456aa9b9c2e8b13ed03417ac6125d3
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2018-09-20 14:29:16 -0500

    Add Server Definitions Unit Tests
    This commit adds unit tests for server definitions.

---
+++ frontend/src/__tests__/utils/definitions/server/serverList.test.js
@@ -0,0 +1,60 @@
+/* global describe */
+import sDefns, { initialServerStates } from '../../../../utils/definitions/serverDefinitions';
+import { setupConsoleErrorSpy, testKey, testArray } from '../../../../__testUtils__/definitionTestUtils';
+
+describe('serverList definitions', () => {
+  const spy = setupConsoleErrorSpy();
+
+  const testServerRowKey = (keyName, valid, invalid) =>
+    testKey(
+      keyName,
+      valid,
+      invalid,
+      sDefns.serverRow,
+      initialServerStates.serverRow,
+      spy,
+    );
+
+  testServerRowKey('charges', [null, 'test'], [{}, 3, false]);
+  testServerRowKey('status', [null, 'test'], [{}, 3, false]);
+  testServerRowKey('action', [null, 'test'], [{}, 3, false]);
+
+  testArray(
+    [
+      { status: 'running' },
+      { charges: 'test' },
+      { action: 'test' },
+      {
+        action: 'test',
+        charges: 'test',
+        status: 'running',
+        type: {
+          id: 1,
+          value: 'test',
+          label: 'test',
+        },
+        size: {
+          id: 1,
+          value: 'test',
+          label: 'test',
+        },
+        location: {
+          id: 1,
+          value: 'test',
+          label: 'test',
+        },
+      },
+    ],
+    [
+      { status: true },
+      { action: true },
+      { charges: true },
+      { type: 'invalid' },
+      { size: 'invalid' },
+      { location: 'invalid' },
+    ],
+    sDefns.serverList,
+    initialServerStates.serverList,
+    spy,
+  );
+});
