8ed966a5fcde5ad5972750c44a472759d9155330
Matthew Wall <matthew.wallt@gmail.com>
2018-09-21 15:29:23 -0400

    server scss overhaul

---
+++ frontend/src/server/old/viewLog.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import ServerRow from './serverRow';
+import defns from '../../utils/definitions/serverDefinitions';
+
+class ViewLog extends Component {
+  constructor(props) {
+    super(props);
+    this.createTable = this.createTable.bind(this);
+  }
+
+  createTable() {
+    const table = this.props.servers.map(server => (<ServerRow key={server.id} server={server} />));
+    return table;
+  }
+
+  render() {
+    return (
+      <div className="server-table">
+        {this.createTable()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  servers: state.servers,
+});
+
+ViewLog.propTypes = {
+  servers: defns.serverList.isRequired,
+};
+
+export default connect(mapStateToProps)(ViewLog);
