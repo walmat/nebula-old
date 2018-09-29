e28c449a54f5611cf58142f6ee34fcf85fd26386
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2018-09-29 04:14:51 -0500

    Combine token and refresh endpoints into token
    This commit combines the token and refresh auth endpoints into just one route: /auth/token. Both endpoints are performing the exact same function -- Given some type of credentials (key or refresh token), return a new jwt access token. 
    
    The request body now requires a "grant_type" key that is either "key" or "refresh" depending on the grant type we are providing. A key grant type expects a "key" key that contains the license key. A "refresh" graph type expects a "token" key that contains the refresh token.

--- nebula-api/src/routes/auth/auth.js
+++ nebula-api/src/routes/auth/auth.js
@@ -28,52 +28,62 @@ module.exports = async function(app) {
     });
 
     app.post('/auth/token', async function(req, res) {
-        // Check for key
-        const { key } = req.body;
-        if (key) {
-            const response = await authUtils.verifyKey(key);
-            if(response.error) {
-                if(response.error.name === 'InternalError') {
-                    // Internal error
-                    return res.status(501).json(response);
-                }
-                // Authentication Error
-                return res.status(401).json(response);
+        console.log(req.body);
+        // Check grant type
+        const { grant_type } = req.body;
+        let value = null;
+        let verify = null;
+        switch(grant_type) {
+            case 'key': {
+                console.log('[DEBUG]: Using key to generate access token...');
+                value = req.body.key;
+                verify = authUtils.verifyKey;
+                break;
             }
-            // Success
-            return res.status(200).json(response);
+            case 'refresh': {
+                console.log('[DEBUG]: Using refresh token to generate access token...');
+                value = req.body.token;
+                verify = authUtils.verifyToken;
+                break;
+            }
+            default: {
+                console.log('[DEBUG]: Grant Type could not be verified');
+                // Bad Request
+                return res.status(400).json({
+                    error: {
+                        name: 'BadRequest',
+                        message: 'Bad Request',
+                    },
+                });
+            }
         }
-        // Bad Request
-        return res.status(400).json({
-            error: {
-                name: 'BadRequest',
-                message: 'Bad Request',
-            },
-        });
-    });
 
-    app.post('/auth/refresh', async function(req, res) {
-        // Check for token
-        const { token } = req.body;
-        if(token) {
-            const response = await authUtils.verifyToken(token);
-            if(response.error) {
-                if (response.error.name === 'InternalError') {
-                    // Internal Error
-                    return res.status(501).json(response);
-                }
-                // Authentication Error
-                return res.status(401).json(response);
-            }
-            // Success
-            return res.status(200).json(response);
+        // Make sure value is available
+        if(!value) {
+            console.log('[DEBUG]: value was not given, returning 400...');
+            // Bad Request
+            return res.status(400).json({
+                error: {
+                    name: 'BadRequest',
+                    message: 'Bad Request',
+                },
+            });
         }
-        // Bad Request
-        return res.status(400).json({
-            error: {
-                name: 'BadRequest',
-                message: 'Bad Request',
-            },
-        });
-    })
+
+        console.log('[DEBUG]: Performing verification...');
+        // Perform Verification
+        const response = await verify(value);
+
+        // Handle Response
+        if(response.error) {
+            if(response.error.name === 'InternalError') {
+                // Internal error
+                return res.status(501).json(response);
+            }
+            // Authentication Error
+            return res.status(401).json(response);
+        }
+        // Success
+        return res.status(200).json(response);
+    });
 };
