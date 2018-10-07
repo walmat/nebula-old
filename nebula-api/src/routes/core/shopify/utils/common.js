1940ec3c9249ac73611768813cbce0b0e788e664
Matthew Wall <matthew.wallt@gmail.com>
2018-10-07 02:24:49 -0400

    task runner updates

--- nebula-api/src/routes/core/shopify/utils/common.js
+++ nebula-api/src/routes/core/shopify/utils/common.js
@@ -1,27 +1,44 @@
 /**
  * a class that contains global common elements
- * @type {{}}
+ * @type {Functions}
  */
-
 module.exports = {};
 
-/**
- * Helper method to format the proxies properly for use
- * @param str
- * @returns {*}
- */
-function formatProxy(str) {
-    //format is ip:port:user:pass
-    let data = str.split(':');
+const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36';
+module.exports.userAgent = userAgent;
 
-    if (data.length === 2) {
-        return 'http://' + data[0] + ':' + data[1]; //proxy with no user/pass
-    } else if (data.length === 4) {
-        return 'http://' + data[2] + ':' + data[3] + '@' + data[0] + ':' + data[1];
+/**
+ * Formats the proxy correctly to be used in a request
+ * @param {*} input - IP:PORT:USER:PASS || IP:PORT
+ * @returns - http://USER:PASS@IP:PORT || http://IP:PORT
+ */
+function formatProxy(input) {
+
+    // safeguard for if it's already formatted or in a format we can't handle
+    if (input === null || input === undefined || input.startsWith('http') || input === 'localhost') {
+        return input;
     } else {
-        console.log('Unable to parse proxy');
-        return null;
+        let data = input.split(':');
+        return (data.length === 2) ? 'http://' + data[0] + ':' + data[1] :
+               (data.length === 4) ? 'http://' + data[2] + ':' + data[3] + '@' + data[0] + ':' + data[1] :
+               null;
     }
 }
-
 module.exports.formatProxy = formatProxy;
+
+/**
+ * Takes in either the pos_keywords/neg_keywords and trims the
+ * +/- off of them while converting them to uppercase ahead of
+ * time to be used in string comparison algorithms.
+ * 
+ * @param {Array} input 
+ * @return {Array} array of trimmed keywords
+ */
+function trimKeywords(input) {
+    let ret = [];
+    input.map(word => {
+        ret.push(word.substring(1, word.length).trim().toUpperCase()); //trim() just in case, probably not necessary
+    });
+    return ret;
+}
+module.exports.trimKeywords = trimKeywords;
