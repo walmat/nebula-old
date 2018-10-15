db9ac0e15be812709af45b35c33f36aaec53d62e
Matthew Wall <Matthew Wall>
2018-10-15 01:26:21 -0400

    fixed css issues for windows

--- frontend/src/utils/validationStatus.js
+++ frontend/src/utils/validationStatus.js
@@ -1,28 +1,15 @@
 import PropTypes from 'prop-types';
-import valid from '../_assets/Symbol_check-01.png';
-// import unknown from '../_assets/Empty_icons-03.svg';
-import invalid from '../_assets/Symbol_check-02.png';
 
 const errorStyle = {
   borderColor: '#EF415E',
-  // backgroundImage: `url(${invalid})`,
-  // backgroundRepeat: 'no-repeat',
-  // backgroundPosition: 'right center',
-  // backgroundOrigin: 'content-box',
-  // backgroundSize: '15px 15px',
 };
 
-const neutralStyle = {
-  borderColor: '#6D6E70',
-};
+// const neutralStyle = {
+//   borderColor: '#6D6E70',
+// };
 
 const validStyle = {
   borderColor: '#46ADB4',
-  // backgroundImage: `url(${valid})`,
-  // backgroundRepeat: 'no-repeat',
-  // backgroundPosition: 'right center',
-  // backgroundOrigin: 'content-box',
-  // backgroundSize: '15px 15px',
 };
 
 function validationStatus(validationErrors) {
