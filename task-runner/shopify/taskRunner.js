98a178ec9b4f0af3762c45eddb8279bbc4df0f17
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2018-10-16 02:37:08 -0500

    Add Core TaskRunner Logic
    This commit adds the core task runner logic that handles calling the sub processes and handling the responses. The logic uses a basic state machine to handle each step and calculate the next state.

--- task-runner/shopify/taskRunner.js
+++ task-runner/shopify/taskRunner.js
@@ -3,8 +3,51 @@ const Checkout = require('./classes/checkout');
 const EventEmitter = require('events');
 
 class TaskRunner {
+    /**
+     * Event Channel Constants
+     */
+    static get Events() {
+        return {
+            All: 'ALL',
+            TaskStatus: 'TASK_STATUS',
+            MonitorStatus: 'MONITOR_STATUS',
+            CheckoutStatus: 'CHECKOUT_STATUS',
+        };
+    };
+
+    /**
+     * Task Runner States
+     */
+    static get States() {
+        return {
+            Initialized: 'INIT',
+            Started: 'STARTED',
+            GenQueueBypass: 'GEN_QUEUE_BYPASS',
+            Monitor: 'MONITOR',
+            SwapProxies: 'SWAP_PROXIES',
+            Checkout: 'CHECKOUT',
+            Finished: 'FINISHED',
+            Aborted: 'ABORTED',
+            Stopped: 'STOPPED',
+        };
+    }
     
-    constructor(task, proxy) {
+    constructor(id, task, proxy) {
+        /**
+         * The id of this task runner
+         */
+        this._id = id;
+
+        /**
+         * The task that should be run
+         */
+        this._task = task;
+
+         /**
+          * The proxy that will be used
+          */
+        this._proxy = proxy;
+
         /**
          * Create a new monitor object to be used for the task
          */
@@ -17,19 +60,203 @@ class TaskRunner {
 
         /**
          * Create a new event emitter to handle all IPC communication
+         * 
+         * Events will provide the task id, a message, and a message group
          */
         this._events = new EventEmitter();
 
+        /**
+         * Internal Task Runner State
+         */
+        this._state = TaskRunner.States.Initialized;
     }
 
-    registerForEvent(event, callback) { 
-        this._events.on(event, callback);
+    // MARK: Event Registration
+
+    registerForEvent(event, callback) {
+        switch(event) {
+            case TaskRunner.Events.TaskStatus: {
+                this._events.on(TaskRunner.Events.TaskStatus, callback);
+                break;
+            }
+            case TaskRunner.Events.MonitorStatus: {
+                this._events.on(TaskRunner.Events.MonitorStatus, callback);
+                break;
+            }
+            case TaskRunner.Events.CheckoutStatus: {
+                this._events.on(TaskRunner.Events.CheckoutStatus, callback);
+                break;
+            }
+            case TaskRunner.Events.All: {
+                this._events.on(TaskRunner.Events.TaskStatus, callback);
+                this._events.on(TaskRunner.Events.MonitorStatus, callback);
+                this._events.on(TaskRunner.Events.CheckoutStatus, callback);
+            }
+        }
     }
 
+    deregisterForEvent(event, callback) {
+        switch(event) {
+            case TaskRunner.Events.TaskStatus: {
+                this._events.removeListener(TaskRunner.Events.TaskStatus, callback);
+                break;
+            }
+            case TaskRunner.Events.MonitorStatus: {
+                this._events.removeListener(TaskRunner.Events.MonitorStatus, callback);
+                break;
+            }
+            case TaskRunner.Events.CheckoutStatus: {
+                this._events.removeListener(TaskRunner.Events.CheckoutStatus, callback);
+                break;
+            }
+            case TaskRunner.Events.All: {
+                this._events.removeListener(TaskRunner.Events.TaskStatus, callback);
+                this._events.removeListener(TaskRunner.Events.MonitorStatus, callback);
+                this._events.removeListener(TaskRunner.Events.CheckoutStatus, callback);
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    // MARK: Event Emitting
+
+    // TEMPORARY
     emitEvent() {
-        this._events.emit('monitor-status', 'initializing...')
+        this._emitEvent(TaskRunner.Events.MonitorStatus, { message: 'initializing...' })
     }
 
+    _emitEvent(event, message) {
+        switch(event) {
+            case TaskRunner.Events.TaskStatus: {
+                this._events.emit(TaskRunner.Events.TaskStatus, this._id, message, TaskRunner.Events.TaskStatus);
+                break;
+            }
+            case TaskRunner.Events.MonitorStatus: {
+                this._events.emit(TaskRunner.Events.MonitorStatus, this._id, message, TaskRunner.Events.MonitorStatus);
+                break;
+            }
+            case TaskRunner.Events.CheckoutStatus: {
+                this._events.emit(TaskRunner.Events.CheckoutStatus, this._id, message, TaskRunner.Events.CheckoutStatus);
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    _emitTaskEvent(message) {
+        _emitEvent(TaskRunner.Events.TaskStatus, message);
+    }
+
+    _emitMonitorEvent(message) {
+        _emitEvent(TaskRunner.Events.MonitorStatus, message);
+    }
+
+    _emitCheckoutEvent(message) {
+        _emitEvent(TaskRunner.Events.CheckoutStatus, message);
+    }
+
+    // MARK: State Machine Step Logic
+
+    _handleStarted() {
+        this._emitTaskEvent({
+            message: 'Task is Starting...',
+        });
+        return TaskRunner.States.GenQueueBypass;
+    }
+
+    _handleGenQueueBypass() {
+        const res = await this._checkout.generateQueueBypassUrl();
+        if(res.errors) {
+            this._emitTaskEvent({
+                message: 'Unable to Generate Bypass Queue Url! Continuing on...',
+                errors: res.errors,
+            });
+        }
+        return TaskRunner.States.Monitor;
+    }
+
+    _handleMonitor() {
+        const res = await this._monitor.run();
+        if(res.errors) {
+            this._emitTaskEvent({
+                message: 'Error with Monitor! Retrying...',
+                errors: res.errors,
+            });
+        }
+        // Monitor will be in charge of choosing the next state
+        return res.nextState;
+    }
+
+    _handleSwapProxies() {
+        const res = await this._taskManager.swapProxies(this._proxy);
+        if (res.errors) {
+            this._emitTaskEvent({
+                message: 'Error Swapping Proxies! Retrying Monitor...',
+                errors: res.errors,
+            });
+        }
+        // Swap Proxies will be in charge of choosing the next state
+        return res.nextState;
+    }
+
+    _handleCheckout() {
+        const res = await this._checkout.run();
+        if (res.errors) {
+            this._emitTaskEvent({
+                message: 'Errors during Checkout! Retrying Monitor...',
+                errors: res.errors,
+            });
+        }
+        // Checkout will be in charge of choosing the next state
+        return res.nextState;
+    }
+
+    _handleFinished() {
+        this._emitTaskEvent({
+            message: 'Task has finished!',
+        });
+        return TaskRunner.States.Stopped;
+    }
+
+    _handleAborted() {
+        this._emitTaskEvent({
+            message: 'Task has aborted!',
+        });
+        return TaskRunner.States.Stopped;
+    }
+
+    _handleStepLogic(currentState) {
+        function defaultHandler() {
+            return currentState;
+        }
+        const stepMap = {
+            [TaskRunner.States.Started]: this._handleStarted,
+            [TaskRunner.States.GenQueueBypass]: this._handleGenQueueBypass,
+            [TaskRunner.States.Monitor]: this._handleMonitor,
+            [TaskRunner.States.SwapProxies]: this._handleSwapProxies,
+            [TaskRunner.States.Checkout]: this._handleCheckout,
+            [TaskRunner.States.Finished]: this._handleFinished,
+            [TaskRunner.States.Aborted]: this._handleAborted,
+        }
+        const handler = stepMap[currentState] || defaultHandler;
+        return handler();
+    }
+
+    // MARK: State Machine Run Loop
+
+    async start() {
+        this._state = TaskRunner.Started;
+        while(this._state !== TaskRunner.States.Stopped) {
+            this._state = this._handleStepLogic(this._state);
+        }
+        this._emitTaskEvent({
+            message: 'Task has stopped.',
+        });
+    }
 }
 
 module.exports = TaskRunner;
