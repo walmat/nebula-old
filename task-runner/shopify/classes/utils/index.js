879afe97d9dc3132d47ea929dcb267c93c79e692
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2018-11-14 03:34:15 -0600

    Add Logger to Utils
    This commit adds support of an optional logger to all util methods. If no logger is passed, then no log operations are done. All console.log statements are converted to use the logger.

--- task-runner/shopify/classes/utils/index.js
+++ task-runner/shopify/classes/utils/index.js
@@ -35,7 +35,6 @@ module.exports.formatProxy = formatProxy;
 function autoParse(body, response, resolveWithFullResponse) {
     // FIXME: The content type string could contain additional values like the charset.
     // Consider using the `content-type` library for a robust comparison.
-    console.log(response);
     if (response.headers['content-type'] === 'application/json') {
         return JSON.parse(body);
     } else if (response.headers['content-type'] === 'text/html') {
