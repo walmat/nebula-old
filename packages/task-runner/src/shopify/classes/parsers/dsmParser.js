e37fee19bfebbeaa797856ffcb5e360432d47fa4
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2018-11-27 19:43:52 -0600

    Add DSM Parser
    This commit adds the first special parser variant for DSM sites.
    
    The parser index file is updated to include a function for retrieving the correct special parser given a site object. This initially just uses the site's name, but a better method should be implemented in the future.

---
+++ packages/task-runner/src/shopify/classes/parsers/dsmParser.js
@@ -0,0 +1,58 @@
+const SpecialParser = require('./specialParser');
+const { ParseType, matchKeywords } = require('../utils/parse');
+
+/**
+ * Special Parser for all DSM sites
+ */
+class DsmParser extends SpecialParser {
+  constructor(task, proxy, logger) {
+    super(task, proxy, logger, 'DsmParser');
+  }
+
+  parseInitialPage($) {
+    // Look for all `.grid-view-item`'s
+    const parsedItems = $('.grid-view-item').map((i, el) => {
+      const link = $('.grid-view-item__link', this).attr('href');
+      const title = $('.grid-view-item__title', this).text();
+
+      if (!link || !title) {
+        return null;
+      }
+      return { link, title };
+    }).filter(i => !!(i));
+
+    if(!items.length) {
+      throw new Error('No Items Found');
+    }
+
+    
+    let items = parsedItems;
+    // If parsing keywords, reduce the number of pages to search by matching the title
+    if (this._type === ParseType.Keywords) {
+      const keywords = {
+        pos: this._task.product.pos_keywords,
+        neg: this._task.product.neg_keywords,
+      }
+      items = matchKeywords(parsedItems, keywords);
+    }
+    this._logger.silly('%s: parsing inital page, found %d items', this._name, items.length);
+
+    // Convert items to full urls
+    const productUrls = items.map(({ link }) => {
+      return new URL(link, this._task.site.url).href;
+    });
+
+    return productUrls;
+  }
+
+  parseProductInfoPage($) {
+    // Look for the script tag containing the product json
+    const product = $('script#ProductJson-product-template');
+    if(!product || product.attr('type') !== 'application/json') {
+      throw new Error('Could not find product data!');
+    }
+    return JSON.parse(product.text());
+  }
+}
+
+module.exports = DsmParser;
--- packages/task-runner/src/shopify/classes/parsers/index.js
+++ packages/task-runner/src/shopify/classes/parsers/index.js
@@ -1,7 +1,32 @@
 // Top Level Export for parsers
+const Parser = require('./parser');
+const AtomParser = require('./atomParser');
+const JsonParser = require('./jsonParser');
+const XmlParser = require('./xmlParser');
+
+// Special Parsers
+const DsmParser = require('./dsmParser');
+
+function getSpecialParser(site) {
+  // TODO: Figure out a better way to do this!
+  switch(site.name) {
+    case 'DSM SG':
+    case 'DSM JP':
+    case 'DSM US':
+    case 'DSM EU': {
+      return DsmParser;
+    }
+    case 'Yeezy Supply':
+    default: {
+      return Parser;
+    }
+  }
+}
+
 module.exports = {
-  Parser: require('./parser'),
-  AtomParser: require('./atomParser'),
-  JsonParser: require('./jsonParser'),
-  XmlParser: require('./xmlParser'),
+  Parser,
+  AtomParser,
+  JsonParser,
+  XmlParser,
+  getSpecialParser,
 };
