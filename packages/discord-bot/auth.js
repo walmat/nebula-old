8e97d9a18513498856139da54b78c24682757f7b
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2018-12-14 21:55:29 -0600

    Fix Discord Bot Linter Errors
    This commit fixes all linter errors in the discord-bot. Only warnings remain.

--- packages/discord-bot/auth.js
+++ packages/discord-bot/auth.js
@@ -1,47 +1,42 @@
 const fetch = require('node-fetch');
 
 async function bind(licenseKey, discordId, cb) {
+  // call the nebula api endpoint
+  const result = await fetch(`${process.env.NEBULA_API_ENDPOINT}/auth/discord`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      discordId,
+      licenseKey,
+    }),
+  });
 
-    // call the nebula api endpoint
-    let result = await fetch(`${process.env.NEBULA_API_ENDPOINT}/auth/discord`,
-        {
-            method: "POST",
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                discordId,
-                licenseKey
-            }),
-        });
-
-    const body = await result.json();
-    if (result.status === 200) {
-        return cb(false, body.message);
-    } else {
-        return cb(true, body.message);
-    }
+  const body = await result.json();
+  if (result.status === 200) {
+    return cb(false, body.message);
+  }
+  return cb(true, body.message);
 }
 
 async function deactivate(licenseKey, discordId, cb) {
+  // call the nebula api endpoint
+  const result = await fetch(`${process.env.NEBULA_API_ENDPOINT}/auth/discord`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      discordId,
+      licenseKey,
+    }),
+  });
 
-    // call the nebula api endpoint
-    let result = await fetch(`${process.env.NEBULA_API_ENDPOINT}/auth/discord`,
-        {
-            method: "DELETE",
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                discordId,
-                licenseKey
-            }),
-        });
-
-    const body = await result.json();
-    return cb(body.message);
+  const body = await result.json();
+  return cb(body.message);
 }
 
 /**
