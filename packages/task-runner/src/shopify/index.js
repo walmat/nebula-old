13b17d447a5488167c1aa46ded388ac1420eced8
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2018-12-19 00:35:03 -0600

    Change Task Runner's Entry Point
    This commit updates the Task-Runner project to export classes in an orderly manner. The entry point defined in the package now allows you to access all relevant classes.

---
+++ packages/task-runner/src/shopify/index.js
@@ -0,0 +1,9 @@
+const TaskManager = require('./taskManager');
+const TaskProcessManager = require('./taskProcessManager');
+const TaskRunner = require('./taskRunner');
+
+module.exports = {
+  TaskManager,
+  TaskProcessManager,
+  TaskRunner,
+};
