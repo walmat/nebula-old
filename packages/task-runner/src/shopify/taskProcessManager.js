1a1e8f7f7c30b7ad1a5afcd74e549e19053ad6af
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2018-12-18 19:15:32 -0600

    Pivot to Process Management
    This commit renames the new TaskManager to better reflect what the manager represents: Instead of multiple threads, the new manager uses multiple child processes.

--- packages/task-runner/src/shopify/taskProcessManager.js
+++ packages/task-runner/src/shopify/taskProcessManager.js
@@ -2,11 +2,11 @@ const { spawn } = require('threads');
 
 const TaskManager = require('./taskManager');
 
-class TaskThreadManager extends TaskManager {
+class TaskProcessManager extends TaskManager {
   async _start([runnerId, task, openProxy]) {
     const thread = spawn(super._start);
     thread.send([runnerId, task, openProxy]);
   }
 }
 
-module.exports = TaskThreadManager;
+module.exports = TaskProcessManager;
