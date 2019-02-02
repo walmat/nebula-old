bbd19774de1e72a226c87d075348ebba24829d2c
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-02-02 00:46:29 -0600

    Stop Tasks that are Requesting Captchas (#297)
    * Move AsyncQueue to Common folder
    
    This commit moves the AsyncQueue class to the common folder. This class
    is a general class that is not shopify dependent, so the common folder
    is a better location for it.
    
    * Add Cancel method to AsyncQueue
    
    This commit updates the async queue with cancel functionality. Instead
    of returning a promise for the next token, a request object is returned.
    This request object includes the promise for the the next token, as well
    as the status of the request, the resolved value, and a cancel function
    to stop the request.
    
    * Handle Aborts in Captcha State Handler
    
    This commit updates the usage of the AsyncQueue to properly cancel a
    token request when the captcha is aborted. Further, the implementation
    of the handler now polls for the fulfilled status of the token instead
    of blocking the flow of execution until the request promise ends. This
    allows us to periodically check for the abort flag so we know when to
    cancel the request.

---
+++ packages/task-runner/src/common/asyncQueue.js
@@ -0,0 +1,93 @@
+/**
+ * Async Queue
+ *
+ * A variant of a Queue with synchronous inserts and
+ * asynchronous returns. If there is a backlog, the
+ * returns are synchronous
+ */
+class AsyncQueue {
+  constructor() {
+    this._backlog = [];
+    this._waitQueue = [];
+  }
+
+  get backlogLength() {
+    return this._backlog.length;
+  }
+
+  get lineLength() {
+    return this._waitQueue.length;
+  }
+
+  insert(datum) {
+    // Check if we have anybody waiting for data
+    if (this._waitQueue.length) {
+      // Get the resolution and invoke it with the data
+      const resolution = this._waitQueue.pop();
+      resolution.request.status = 'fulfilled';
+      resolution.request.value = datum;
+      resolution.resolve(datum);
+    } else {
+      // Add data to the backlog
+      this._backlog.unshift(datum);
+    }
+    return this._backlog.length;
+  }
+
+  next() {
+    // initialize request
+    const nextRequest = {
+      status: 'pending', // status of the request
+      cancel: null, // function to cancel the request with a given reason
+      promise: null, // the async promise that is waiting for the next value
+      reason: '', // the reason for cancelling the request
+      value: null, // the resolved value
+    };
+
+    // Check if we don't have any waiters and we do have a backlog
+    if (!this._waitQueue.length && this._backlog.length) {
+      // return from the backlog immediately
+      const value = this._backlog.pop();
+      const promise = Promise.resolve(value);
+      return {
+        ...nextRequest,
+        status: 'fulfilled',
+        promise,
+        value,
+      };
+    }
+
+    // Setup request promise and cancel function
+    nextRequest.promise = new Promise((resolve, reject) => {
+      nextRequest.cancel = reason => {
+        nextRequest.status = 'cancelled';
+        nextRequest.reason = reason;
+        this._waitQueue = this._waitQueue.filter(r => r.request !== nextRequest);
+        reject(reason);
+      };
+      this._waitQueue.push({ resolve, reject, request: nextRequest });
+    });
+
+    return nextRequest;
+  }
+
+  clear() {
+    // Remove all items from the backlog if they exist
+    this._backlog = [];
+  }
+
+  destroy() {
+    // Reject all resolutions in the wait queue
+    this._waitQueue.forEach(({ reject, request }) => {
+      request.status = 'destroyed';
+      request.reason = 'Queue was destroyed';
+      reject('Queue was destroyed');
+    });
+    this._waitQueue = [];
+
+    // Remove all items from the backlog if they exist
+    this.clear();
+  }
+}
+
+module.exports = AsyncQueue;
