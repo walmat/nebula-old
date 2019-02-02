f8da31ae9e8dd2da9190111bc8b8ff77d8f052a5
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-02-02 02:51:09 -0600

    Store Refresh Expiration in Users Table
    This commit updates the users table to store the expiration of the
    refresh token. This helps to prevent a bug that locks up a users key
    if they don't authenticate within the refresh token period.

--- packages/nebula-api/dynamoDBUser.js
+++ packages/nebula-api/dynamoDBUser.js
@@ -1,42 +1,42 @@
-var AWS = require("aws-sdk");
-const crypto = require('crypto');
+const AWS = require('aws-sdk');
 require('./src/utils/env').setUpEnvironment();
 const config = require('./src/utils/setupDynamoConfig').getConfig();
 
 const { hash } = require('./hash');
 const { salt, algo, output } = require('./hashConfig.json');
 
-async function storeUser(keyHash) {
+async function storeUser(keyHash, refreshToken, expiry) {
   AWS.config = new AWS.Config(config);
   const dynamodb = new AWS.DynamoDB();
   const keyId = hash(algo, keyHash, salt, output);
-  const refreshToken = await new Promise((resolve) => {
-    crypto.randomBytes(48, (err, buffer) => {
-      resolve(buffer.toString('hex'));
-    });
-  });
   const params = {
     Item: {
-      "keyId": {
+      keyId: {
         S: keyId,
       },
-      "refresh_token": {
+      refresh_token: {
         S: refreshToken,
       },
-    }, 
-    ReturnConsumedCapacity: "TOTAL", 
-    TableName: "Users"
-  };
-  return await dynamodb.putItem(params).promise().then(
-    (data) => {
-      console.log('[SUCCESS]: Successfully added new user');
-      return refreshToken;
+      expiry: {
+        N: `${expiry}`,
+      },
     },
-    (err) => {
-      console.log('[ERROR]: ', err, err.stack);
-      return null;
-    }
-  );
+    ReturnConsumedCapacity: 'TOTAL',
+    TableName: 'Users',
+  };
+  return dynamodb
+    .putItem(params)
+    .promise()
+    .then(
+      () => {
+        console.log('[SUCCESS]: Successfully added new user');
+        return refreshToken;
+      },
+      err => {
+        console.log('[ERROR]: ', err, err.stack);
+        throw new Error(err.message);
+      },
+    );
 }
 
 async function deleteUser(keyHash) {
@@ -45,22 +45,25 @@ async function deleteUser(keyHash) {
   const keyId = hash(algo, keyHash, salt, output);
   const params = {
     Key: {
-      "keyId": {
+      keyId: {
         S: keyId,
       },
     },
-    ReturnConsumedCapacity: "TOTAL", 
-    TableName: "Users"
+    ReturnConsumedCapacity: 'TOTAL',
+    TableName: 'Users',
   };
-  return await dynamodb.deleteItem(params).promise().then(
-    (data) => {
-      console.log('[SUCCESS]: Successfully deleted user');
-      return true;
-    },
-    (err) => {
-      console.log('[ERROR]: ', err, err.stack);
-      return false;
-    }
-  );
+  return dynamodb
+    .deleteItem(params)
+    .promise()
+    .then(
+      () => {
+        console.log('[SUCCESS]: Successfully deleted user');
+        return true;
+      },
+      err => {
+        console.log('[ERROR]: ', err, err.stack);
+        return false;
+      },
+    );
 }
 module.exports = { storeUser, deleteUser };
