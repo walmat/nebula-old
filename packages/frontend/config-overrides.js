8ff15f4f9d05d958c9cb3c6062777a69aab26a38
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-02-05 23:41:56 -0600

    Update React App Rewired Config
    This commit updates the version of react app rewired to work with
    react-scripts ^2.0.0. The hack that was used to properly load svg
    imports broke due to this, so a new update to the config-overrides
    is implemented to restore svg loading.

--- packages/frontend/config-overrides.js
+++ packages/frontend/config-overrides.js
@@ -2,5 +2,27 @@
 const rewireYarnWorkspaces = require('react-app-rewire-yarn-workspaces');
 
 module.exports = function override(config, env) {
-  return rewireYarnWorkspaces(config, env);
+  const overwrittenConfig = { ...config };
+  if (env === 'production') {
+    const { rules } = config.module;
+    // Looks like rules.map is undefined, so we need to use a for loop here
+    // This hack will go away once we start using svgs correctly (See issue #308)
+    // eslint-disable-next-line no-restricted-syntax
+    for (const rule of rules) {
+      if (rule.oneOf) {
+        const newOneOfList = rule.oneOf.slice(0, -1);
+        newOneOfList.push({
+          loader: require.resolve('url-loader'),
+          exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+          options: {
+            name: 'static/media/[name].[hash:8].[ext]',
+          },
+        });
+        rule.oneOf = newOneOfList;
+      }
+    }
+    overwrittenConfig.module.rules = config.module.rules;
+  }
+
+  return rewireYarnWorkspaces(overwrittenConfig, env);
 };
