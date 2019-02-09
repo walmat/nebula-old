2814990ff749417f8706949ef9a5237a3b12bcaf
Matthew Wall <matthew.wallt@gmail.com>
2019-02-08 19:09:06 -0500

    updated navbar resposiveness

--- packages/frontend/src/navbar/navbar.jsx
+++ packages/frontend/src/navbar/navbar.jsx
@@ -30,7 +30,7 @@ export class NavbarPrimitive extends PureComponent {
     if (window.Bridge) {
       return window.Bridge.getAppData();
     }
-    return { name: 'Nebula-Orion', version: null };
+    return { name: 'Nebula Orion', version: null };
   }
 
   render() {
@@ -46,85 +46,135 @@ export class NavbarPrimitive extends PureComponent {
     const { name, version } = NavbarPrimitive._getAppData();
 
     return (
-      <div className="nav-container">
-        <div className="flex-column">
-          <Bodymovin options={bodymovinOptions} />
-          <div id="vert-line" />
-          <div
-            role="button"
-            tabIndex={0}
-            title="TASKS"
-            onKeyPress={onKeyPress}
-            className={
-              navbar.location === '/' || navbar.location === ROUTES.TASKS ? 'active' : null
-            }
-            onClick={() => {
-              onRouteTasks(history);
-            }}
-          >
-            <img src={tasks} className="main-icons" id="icon-tasks" alt="tasks" draggable="false" />
+      <div className="container navbar">
+        {/* <div className="navbar separator" /> */}
+        <div className="col col--start">
+          <div className="row--start">
+            <div className="col col--between">
+              <Bodymovin options={bodymovinOptions} />
+            </div>
           </div>
-          <div
-            role="button"
-            tabIndex={0}
-            title="PROFILES"
-            onKeyPress={onKeyPress}
-            className={navbar.location === ROUTES.PROFILES ? 'active' : null}
-            onClick={() => {
-              onRouteProfiles(history);
-            }}
-          >
-            <img
-              src={profiles}
-              className="main-icons"
-              id="icon-profiles"
-              alt="profiles"
-              draggable="false"
-            />
+          <div className="row--tasks">
+            <div className="col col--between">
+              <div className="row row--around">
+                <div className="icons icons--tasks">
+                  <div
+                    role="button"
+                    tabIndex={0}
+                    title="TASKS"
+                    onKeyPress={onKeyPress}
+                    className={
+                      navbar.location === '/' || navbar.location === ROUTES.TASKS ? 'active' : null
+                    }
+                    onClick={() => {
+                      onRouteTasks(history);
+                    }}
+                  >
+                    <img
+                      src={tasks}
+                      className="main-icons__tasks"
+                      id="icon-tasks"
+                      alt="tasks"
+                      draggable="false"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div
-            role="button"
-            tabIndex={0}
-            title="SERVERS"
-            // title="DISABLED"
-            onKeyPress={onKeyPress}
-            className="disabled"
-            // className={navbar.location === ROUTES.SERVER ? 'active' : null}
-            // onClick={() => {
-            //   onRouteServer(history);
-            // }}
-          >
-            <img
-              src={serverDisabled}
-              className="main-icons"
-              id="icon-server"
-              alt="server"
-              draggable="false"
-            />
+          <div className="row--profiles">
+            <div className="col col--between">
+              <div className="row row--around">
+                <div className="icons icons--profiles">
+                  <div
+                    role="button"
+                    tabIndex={0}
+                    title="PROFILES"
+                    onKeyPress={onKeyPress}
+                    className={navbar.location === ROUTES.PROFILES ? 'active' : null}
+                    onClick={() => {
+                      onRouteProfiles(history);
+                    }}
+                  >
+                    <img
+                      src={profiles}
+                      className="main-icons"
+                      id="icon-profiles"
+                      alt="profiles"
+                      draggable="false"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div
-            role="button"
-            tabIndex={0}
-            title="SETTINGS"
-            onKeyPress={onKeyPress}
-            className={navbar.location === ROUTES.SETTINGS ? 'active' : null}
-            onClick={() => {
-              onRouteSettings(history);
-            }}
-          >
-            <img
-              src={settings}
-              className="main-icons"
-              id="icon-settings"
-              alt="settings"
-              draggable="false"
-            />
+          <div className="row--servers">
+            <div className="col col--between">
+              <div className="row row--around">
+                <div className="icons icons--servers">
+                  <div
+                    role="button"
+                    tabIndex={0}
+                    title="SERVERS"
+                    // title="DISABLED"
+                    onKeyPress={onKeyPress}
+                    className="disabled"
+                    // className={navbar.location === ROUTES.SERVER ? 'active' : null}
+                    // onClick={() => {
+                    //   onRouteServer(history);
+                    // }}
+                  >
+                    <img
+                      src={serverDisabled}
+                      className="main-icons"
+                      id="icon-server"
+                      alt="server"
+                      draggable="false"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="appName">
-            <p>{name.replace('-', ' ')}</p>
+          <div className="row--settings">
+            <div className="col col--between">
+              <div className="row row--around">
+                <div className="icons icons--settings">
+                  <div
+                    role="button"
+                    tabIndex={0}
+                    title="SETTINGS"
+                    onKeyPress={onKeyPress}
+                    className={navbar.location === ROUTES.SETTINGS ? 'active' : null}
+                    onClick={() => {
+                      onRouteSettings(history);
+                    }}
+                  >
+                    <img
+                      src={settings}
+                      className="main-icons"
+                      id="icon-settings"
+                      alt="settings"
+                      draggable="false"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="appVersion">
-            <p>{version}</p>
+          <div className="row row--end">
+            <div className="col col--between">
+              <div className="row row--start">
+                <div className="app__name">
+                  <p>{name.replace('-', ' ')}</p>
+                </div>
+              </div>
+              <div className="row row--end">
+                <div className="app__version">
+                  <p>{version}</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- packages/frontend/tool/compile_css.sh
+++ packages/frontend/tool/compile_css.sh
@@ -4,7 +4,7 @@
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 
 # Go into the frontend project
-cd "$ROOT_DIR/frontend"
+cd "$ROOT_DIR/packages/frontend"
 
 # Compile Sass
 
@@ -12,3 +12,4 @@ cd "$ROOT_DIR/frontend"
 # We should figure out a way to recursively do this
 ./node_modules/.bin/sass src/tasks/tasks.scss src/tasks/tasks.css
 ./node_modules/.bin/sass src/server/server.scss src/server.server.css
+./node_modules/.bin/sass src/navbar/navbar.scss src/navbar/navbar.css
