d7d4ce42e74ff7f3deedf9263ff3ae13fdb44842
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-02-11 16:28:34 -0600

    Update Special Parser Selections
    This commit updates the getSpecialParser method to include the
    refactored DsmParser as well as the new variants for specific sites.
    
    The specialParser is also updated slightly to allow for asynchronous
    implementations when parsing the initial/product pages. This allows the
    DsmUkParser to function properly since it has to fetch the custom.js
    file for parsing the hash property.

--- packages/task-runner/src/shopify/classes/parsers/index.js
+++ packages/task-runner/src/shopify/classes/parsers/index.js
@@ -7,20 +7,25 @@ const XmlParser = require('./xmlParser');
 const MOCK_SPECIAL_PARSER = process.env.NEBULA_RUNNER_MOCK_SPECIAL_PARSER || 'DSM US'; // Use the mock special parser from environment or default to DSM
 
 // Special Parsers
-const DsmParser = require('./dsmParser');
+const { DsmParser, DsmUsParser, DsmUkParser } = require('./dsm');
 const YeezyParser = require('./yeezyParser');
 
 function getSpecialParser(site) {
   // TODO: Figure out a better way to do this!
   switch (site.name) {
     case 'Mock Server': {
+      console.log('Mock Server Picked!');
       return getSpecialParser({ name: MOCK_SPECIAL_PARSER });
     }
     case 'DSM SG':
-    case 'DSM JP':
-    case 'DSM US':
-    case 'DSM UK': {
+    case 'DSM JP': {
       return (...params) => new DsmParser(...params);
+    }
+    case 'DSM US': {
+      return (...params) => new DsmUsParser(...params);
+    }
+    case 'DSM UK': {
+      return (...params) => new DsmUkParser(...params);
     }
     case 'Yeezy Supply':
     case 'Yeezy Supply 350':
