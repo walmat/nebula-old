682c4ff496294941925836766db3988fb96869e5
Matthew Wall <matthew.wallt@gmail.com>
2019-02-13 22:15:22 -0500

    start app themes

--- packages/frontend/tool/compile_css.sh
+++ packages/frontend/tool/compile_css.sh
@@ -4,11 +4,12 @@
 ROOT_DIR="$(git rev-parse --show-toplevel)"
 
 # Go into the frontend project
-cd "$ROOT_DIR/frontend"
+cd "$ROOT_DIR/packages/frontend"
 
 # Compile Sass
 
 # Right now we're just adding file manually, but eventually
 # We should figure out a way to recursively do this
-./node_modules/.bin/sass src/tasks/tasks.scss src/tasks/tasks.css
-./node_modules/.bin/sass src/server/server.scss src/server.server.css
+./node_modules/.bin/sass --no-source-map src/tasks/tasks.scss src/tasks/tasks.css
+./node_modules/.bin/sass --no-source-map src/server/server.scss src/server/server.css
+./node_modules/.bin/sass --no-source-map src/app.scss src/app.css
