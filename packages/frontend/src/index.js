8fdb8909a87dfec5660e1d34c84a378609db4156
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-03-11 20:26:09 -0500

    Call Migrate State
    This commit updates the entry point of the frontend react to call migrate state.

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -2,8 +2,11 @@ import ReactDOM from 'react-dom';
 import createApp from './app';
 // import registerServiceWorker from './registerServiceWorker';
 import configureStore from './state/configureStore';
+import { globalActions } from './state/actions';
 
 const store = configureStore();
+
+store.dispatch(globalActions.migrateState());
 
 ReactDOM.render(createApp(store), document.getElementById('root'));
 // registerServiceWorker();
