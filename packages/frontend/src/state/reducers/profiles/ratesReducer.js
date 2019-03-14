810f25613ffa1593b69e60eeecbb98905d2ef8a2
Matthew Wall <matthew.wallt@gmail.com>
2019-03-14 19:51:43 -0400

    filter names and rate input autofilled based on selection done

--- packages/frontend/src/state/reducers/profiles/ratesReducer.js
+++ packages/frontend/src/state/reducers/profiles/ratesReducer.js
@@ -1,23 +1,31 @@
 import { initialProfileStates } from '../../../utils/definitions/profileDefinitions';
-import { mapProfileFieldToKey } from '../../actions';
+import { mapRateFieldToKey, RATES_FIELDS } from '../../actions';
 
 const ratesReducer = (state = initialProfileStates.rates, action) => {
   let change = {};
-  // If we can't map the field to a rates key, don't change anything
-  if (!mapProfileFieldToKey[action.type]) {
+  const nextState = JSON.parse(JSON.stringify(state));
+  // If we can't map the field to a rates key or there's no value, don't change anything
+  if (!mapRateFieldToKey[action.type] || !action.value) {
     return Object.assign({}, state);
   }
   switch (action.type) {
+    case RATES_FIELDS.RATE:
+      if (!action.value.site || !action.value.rate) {
+        break;
+      }
+      change = nextState.find(s => s.site.url === action.value.site.value);
+      change.selectedRate = action.value.rate;
+      break;
     default: {
       change = {
-        [mapProfileFieldToKey[action.type]]:
-          action.value || initialProfileStates.rates[mapProfileFieldToKey[action.type]],
+        [mapRateFieldToKey[action.type]]:
+          action.value || initialProfileStates.rates[mapRateFieldToKey[action.type]],
         errors: action.errors || state.errors,
       };
       break;
     }
   }
 
-  return Object.assign({}, state, change);
+  return nextState;
 };
 export default ratesReducer;
