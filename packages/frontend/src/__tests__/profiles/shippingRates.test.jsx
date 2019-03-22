5a8e19a578b1e66ddaafe0876f6c9e6b13470035
Matthew Wall <matthew.wallt@gmail.com>
2019-03-22 14:15:51 -0400

    shipping rates component tests

--- packages/frontend/src/__tests__/profiles/shippingRates.test.jsx
+++ packages/frontend/src/__tests__/profiles/shippingRates.test.jsx
@@ -35,6 +35,26 @@ describe('<ShippingRates />', () => {
       theme: initialState.theme,
       value: {
         ...initialProfileStates.profile,
+        rates: [...initialProfileStates.rates],
+      },
+      onChange: () => {},
+      onDeleteShippingRate: () => {},
+    };
+  });
+
+  it('should render with required props', () => {
+    const wrapper = renderShallowWithProps();
+    expect(wrapper).toBeDefined();
+    expect(wrapper.find('.profiles-rates__input-group--site')).toHaveLength(0);
+    expect(wrapper.find('.profiles-rates__input-group--name')).toHaveLength(0);
+    expect(wrapper.find('.profiles-rates__input-group--rate')).toHaveLength(0);
+    expect(wrapper.find('.profiles-rates__input-group--delete')).toHaveLength(0);
+  });
+
+  it('should render with custom props', () => {
+    const customProps = {
+      value: {
+        ...initialProfileStates.profile,
         rates: [
           ...initialProfileStates.rates,
           {
@@ -52,17 +72,364 @@ describe('<ShippingRates />', () => {
           },
         ],
       },
-      onChange: () => {},
-      onDeleteShippingRate: () => {},
     };
-  });
-
-  it('should render with required props', () => {
-    const wrapper = renderShallowWithProps();
+    const wrapper = renderShallowWithProps(customProps);
     expect(wrapper).toBeDefined();
+    expect(wrapper.find('.profiles-rates__input-group--rate')).toHaveLength(1);
     expect(wrapper.find('.profiles-rates__input-group--site')).toHaveLength(1);
     expect(wrapper.find('.profiles-rates__input-group--name')).toHaveLength(1);
-    expect(wrapper.find('.profiles-rates__input-group--rate')).toHaveLength(1);
+    expect(wrapper.find('.profiles-rates__input-group--delete')).toHaveLength(1);
+  });
+
+  describe('should render rate fields', () => {
+    test('when no selectedSite is chosen', () => {
+      const customProps = {
+        value: {
+          ...initialProfileStates.profile,
+          selectedSite: null,
+          rates: [
+            ...initialProfileStates.rates,
+            {
+              site: {
+                name: 'Kith',
+                url: 'https://kith.com',
+              },
+              rates: [
+                {
+                  name: '5-7 Business Days',
+                  rate: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+                },
+              ],
+              selectedRate: null,
+            },
+          ],
+        },
+      };
+      const wrapper = renderShallowWithProps(customProps);
+      expect(wrapper).toBeDefined();
+      const rate = wrapper.find('.profiles-rates__input-group--rate');
+      const site = wrapper.find('.profiles-rates__input-group--site');
+      const name = wrapper.find('.profiles-rates__input-group--name');
+      const deleteButton = wrapper.find('.profiles-rates__input-group--delete');
+
+      expect(rate).toHaveLength(1);
+      expect(site).toHaveLength(1);
+      expect(name).toHaveLength(1);
+      expect(deleteButton).toHaveLength(1);
+      expect(site.prop('value')).toBe(null);
+      expect(name.prop('value')).toBe(null);
+      expect(rate.prop('value')).toBe('');
+    });
+
+    test('when selectedSite is chosen and no rate has been chosen', () => {
+      const customProps = {
+        value: {
+          ...initialProfileStates.profile,
+          selectedSite: {
+            label: 'Kith',
+            value: 'https://kith.com',
+          },
+          rates: [
+            ...initialProfileStates.rates,
+            {
+              site: {
+                name: 'Kith',
+                url: 'https://kith.com',
+              },
+              rates: [
+                {
+                  name: '5-7 Business Days',
+                  rate: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+                },
+              ],
+              selectedRate: null,
+            },
+          ],
+        },
+      };
+      const wrapper = renderShallowWithProps(customProps);
+      expect(wrapper).toBeDefined();
+      const rate = wrapper.find('.profiles-rates__input-group--rate');
+      const site = wrapper.find('.profiles-rates__input-group--site');
+      const name = wrapper.find('.profiles-rates__input-group--name');
+      const deleteButton = wrapper.find('.profiles-rates__input-group--delete');
+
+      expect(rate).toHaveLength(1);
+      expect(site).toHaveLength(1);
+      expect(name).toHaveLength(1);
+      expect(deleteButton).toHaveLength(1);
+      expect(site.prop('value')).toEqual({
+        label: 'Kith',
+        value: 'https://kith.com',
+      });
+      expect(name.prop('value')).toBe(null);
+      expect(rate.prop('value')).toBe('');
+    });
+
+    test('when both selectedSite and selectedRate are chosen', () => {
+      const customProps = {
+        value: {
+          ...initialProfileStates.profile,
+          selectedSite: {
+            label: 'Kith',
+            value: 'https://kith.com',
+          },
+          rates: [
+            ...initialProfileStates.rates,
+            {
+              site: {
+                name: 'Kith',
+                url: 'https://kith.com',
+              },
+              rates: [
+                {
+                  name: '5-7 Business Days',
+                  rate: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+                },
+              ],
+              selectedRate: {
+                label: '5-7 Business Days',
+                value: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+              },
+            },
+          ],
+        },
+      };
+      const wrapper = renderShallowWithProps(customProps);
+      expect(wrapper).toBeDefined();
+      const rate = wrapper.find('.profiles-rates__input-group--rate');
+      const site = wrapper.find('.profiles-rates__input-group--site');
+      const name = wrapper.find('.profiles-rates__input-group--name');
+      const deleteButton = wrapper.find('.profiles-rates__input-group--delete');
+
+      expect(rate).toHaveLength(1);
+      expect(site).toHaveLength(1);
+      expect(name).toHaveLength(1);
+      expect(deleteButton).toHaveLength(1);
+      expect(site.prop('value')).toEqual({
+        label: 'Kith',
+        value: 'https://kith.com',
+      });
+      expect(name.prop('value')).toEqual({
+        label: '5-7 Business Days',
+        value: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+      });
+      expect(rate.prop('value')).toEqual('shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00');
+    });
+  });
+
+  describe('should call correct event handler when', () => {
+    test('selecting site', () => {
+      const customProps = {
+        value: {
+          ...initialProfileStates.profile,
+          selectedSite: null,
+          rates: [
+            ...initialProfileStates.rates,
+            {
+              site: {
+                name: 'Kith',
+                url: 'https://kith.com',
+              },
+              rates: [
+                {
+                  name: '5-7 Business Days',
+                  rate: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+                },
+              ],
+              selectedRate: {
+                label: '5-7 Business Days',
+                value: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+              },
+            },
+          ],
+        },
+        onChange: jest.fn(),
+      };
+
+      const wrapper = renderShallowWithProps(customProps);
+      const site = wrapper.find('.profiles-rates__input-group--site');
+      site.simulate('change', {
+        name: 'Kith',
+        url: 'https://kith.com',
+      });
+      expect(customProps.onChange).toHaveBeenCalledWith(
+        {
+          field: RATES_FIELDS.SITE,
+          value: {
+            name: 'Kith',
+            url: 'https://kith.com',
+          },
+        },
+        PROFILE_FIELDS.EDIT_SELECTED_SITE,
+      );
+    });
+
+    test('default', () => {
+      const customProps = {
+        value: {
+          ...initialProfileStates.profile,
+          selectedSite: {
+            label: 'Kith',
+            value: 'https://kith.com',
+          },
+          rates: [
+            ...initialProfileStates.rates,
+            {
+              site: {
+                name: 'Kith',
+                url: 'https://kith.com',
+              },
+              rates: [
+                {
+                  name: '5-7 Business Days',
+                  rate: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+                },
+              ],
+              selectedRate: {
+                label: '5-7 Business Days',
+                value: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+              },
+            },
+          ],
+        },
+        onChange: jest.fn(),
+      };
+
+      const wrapper = renderShallowWithProps(customProps);
+      const name = wrapper.find('.profiles-rates__input-group--name');
+      name.simulate('change', {
+        label: '5-7 Business Days',
+        value: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+      });
+      expect(customProps.onChange).toHaveBeenCalledWith(
+        {
+          field: RATES_FIELDS.RATE,
+          value: {
+            site: {
+              label: 'Kith',
+              value: 'https://kith.com',
+            },
+            rate: {
+              label: '5-7 Business Days',
+              value: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+            },
+          },
+        },
+        PROFILE_FIELDS.EDIT_RATES,
+      );
+    });
+
+    test('deleting when there is no selectedSite', () => {
+      const customProps = {
+        value: {
+          ...initialProfileStates.profile,
+          selectedSite: null,
+          rates: [
+            ...initialProfileStates.rates,
+            {
+              site: {
+                name: 'Kith',
+                url: 'https://kith.com',
+              },
+              rates: [
+                {
+                  name: '5-7 Business Days',
+                  rate: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+                },
+              ],
+              selectedRate: null,
+            },
+          ],
+        },
+        onDeleteShippingRate: jest.fn(),
+      };
+
+      const wrapper = renderShallowWithProps(customProps);
+      const deleteButton = wrapper.find('.profiles-rates__input-group--delete');
+      deleteButton.simulate('click');
+      expect(customProps.onDeleteShippingRate).not.toHaveBeenCalled();
+    });
+
+    test('deleting when there is a selected rate', () => {
+      const customProps = {
+        value: {
+          ...initialProfileStates.profile,
+          selectedSite: {
+            label: 'Kith',
+            value: 'https://kith.com',
+          },
+          rates: [
+            ...initialProfileStates.rates,
+            {
+              site: {
+                name: 'Kith',
+                url: 'https://kith.com',
+              },
+              rates: [
+                {
+                  name: '5-7 Business Days',
+                  rate: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+                },
+              ],
+              selectedRate: {
+                label: '5-7 Business Days',
+                value: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+              },
+            },
+          ],
+        },
+        onDeleteShippingRate: jest.fn(),
+      };
+
+      const wrapper = renderShallowWithProps(customProps);
+      const deleteButton = wrapper.find('.profiles-rates__input-group--delete');
+      deleteButton.simulate('click');
+      expect(customProps.onDeleteShippingRate).toHaveBeenCalledWith(
+        {
+          label: 'Kith',
+          value: 'https://kith.com',
+        },
+        {
+          label: '5-7 Business Days',
+          value: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+        },
+      );
+    });
+
+    test('deleting when there is not a selected rate', () => {
+      const customProps = {
+        value: {
+          ...initialProfileStates.profile,
+          selectedSite: {
+            label: 'Kith',
+            value: 'https://kith.com',
+          },
+          rates: [
+            ...initialProfileStates.rates,
+            {
+              site: {
+                name: 'Kith',
+                url: 'https://kith.com',
+              },
+              rates: [
+                {
+                  name: '5-7 Business Days',
+                  rate: 'shopify-UPS%20GROUND%20(5-7%20business%20days)-10.00',
+                },
+              ],
+              selectedRate: null,
+            },
+          ],
+        },
+        onDeleteShippingRate: jest.fn(),
+      };
+
+      const wrapper = renderShallowWithProps(customProps);
+      const deleteButton = wrapper.find('.profiles-rates__input-group--delete');
+      deleteButton.simulate('click');
+      expect(customProps.onDeleteShippingRate).not.toHaveBeenCalled();
+    });
   });
 
   test('map state to props returns correct structure', () => {
