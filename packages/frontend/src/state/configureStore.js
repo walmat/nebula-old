cffb309715cee0f98cc7779b6323b22f2c053c3b
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-03-28 09:54:51 -0500

    Fix Initial State Migration Bug (#399)
    * Fix Initial State Migration Bug
    
    This commit fixes a bug where the merging of the
    initial state and the persisted state when creating
    the redux store caused the latest version to get
    attached to the old version. This prevented the
    migration from functioning properly since the
    version was the latest even though the data was not.
    
    Now, the initial state isn't given when creating
    the store, so the only starting state used comes
    from the persisted local storage state (if it exists).
    The top level reducer uses the initial state if the
    incoming state is null. This does not affect any other
    parts of the frontend but should prevent migration
    issues when the persisted state is v0.0.0
    
    * Run migrator on init action
    
    This commit updates the top level reducer to run the
    migrators on the init action. This should allow us to
    properly initialize the store using the migrator without
    having to go through full reducer cycle.

--- packages/frontend/src/state/configureStore.js
+++ packages/frontend/src/state/configureStore.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import persistState from 'redux-localstorage';
 import topLevelReducer from './reducers';
-import { initialState } from './migrators';
 import profileAttributeValidationMiddleware from './middleware/profiles/profileAttributeValidationMiddleware';
 import profileFormValidationMiddleware from './middleware/profiles/profileFormValidationMiddleware';
 import settingsAttributeValidationMiddleware from './middleware/settings/settingsAttributeValidationMiddleware';
@@ -14,7 +13,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default function configureStore() {
   return createStore(
     topLevelReducer,
-    initialState,
+    null,
     composeEnhancers(
       applyMiddleware(
         profileAttributeValidationMiddleware,
