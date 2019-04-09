01b7d5114063e671fa8df89f029a2b1cb024249a
Matthew Wall <matthew.wallt@gmail.com>
2019-04-08 23:24:44 -0400

    Apply suggestions from code review

--- packages/frontend/public/js/auth.js
+++ packages/frontend/public/js/auth.js
@@ -5,7 +5,7 @@ window.onload = () => {
     loop: true,
     prerender: true,
     autoplay: true,
-    path: './nebula.json',
+    path: './js/nebula.json',
   };
   const anim = bodymovin.loadAnimation(animData);
   const close = document.getElementById('close');
