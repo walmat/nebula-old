94b0b56d5714ddb2798683c36a9726338cfffbba
Matthew Wall <Matthew@Matthews-MacBook.local>
2019-04-23 17:53:55 -0400

    menu bug fixed + some small changes to about/auth page

--- packages/frontend/public/js/auth.js
+++ packages/frontend/public/js/auth.js
@@ -10,14 +10,6 @@ window.onload = () => {
   const anim = bodymovin.loadAnimation(animData);
   const close = document.getElementById('close');
   const submit = document.getElementById('license-key-submit');
-  const input = document.getElementById('license-key-input');
-
-  input.onkeypress = e => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      checkLicense();
-    }
-  };
 
   close.onclick = window.Bridge.close;
   submit.onclick = checkLicense;
