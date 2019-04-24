4d7489669d36fe138b023be78125f19e09aaaeb1
Matthew Wall <Matthew@Matthews-MacBook.local>
2019-04-23 20:15:31 -0400

    rm unnecessary state changes

--- packages/frontend/src/state/migrators/v0.4.0/state.js
+++ packages/frontend/src/state/migrators/v0.4.0/state.js
@@ -1,9 +1,5 @@
 import prevState from '../v0.3.1/state';
 
-const newState = {
-  ...prevState,
-  version: '0.4.0',
-  tasks: prevState.tasks.filter(t => `${t.index}`.indexOf('-') === -1),
-};
+const newState = { ...prevState, version: '0.4.0' };
 
 export default newState;
