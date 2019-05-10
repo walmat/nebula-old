fe5c43dbd8a7afc8dedab019d0ebd9bf9019ae38
Matthew Wall <matthew.wallt@gmail.com>
2019-05-10 17:48:12 -0400

    added all rates in constants file

---
+++ packages/frontend/src/constants/dsmlRates.js
@@ -0,0 +1,390 @@
+// THESE COULD CHANGE, BE MINDFUL OF THIS GOING FORWARD:
+export default {
+  US: {
+    rate: 'shopify-Express%201-3%20working%20days%20from%20dispatch-20.00',
+    price: '20.00',
+    name: 'Express 1-3 Working Days From Dispatch',
+  },
+  // EU
+  UK: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-5.00',
+    price: '5.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  BE: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  FR: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  DE: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  IT: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  LU: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  NL: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  DK: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  IE: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  GR: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  PT: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  ES: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  AT: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  FI: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  SE: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  EE: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  CY: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  HU: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  LV: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  LT: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  MT: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  PL: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  SK: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  SI: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  BG: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  RO: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  HR: {
+    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
+    price: '15.00',
+    name: 'Express 1-2 Working Days From Dispatch',
+  },
+  // asia
+  SG: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  AF: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  AM: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  AZ: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  BH: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  BD: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  BT: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  BN: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  KH: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  CN: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  GE: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  IN: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  ID: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  IR: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  IQ: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  IL: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  JP: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  JO: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  KZ: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  KW: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  KG: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  LS: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  LB: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  MY: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  MV: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  MN: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  NM: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  MM: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  NP: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  KP: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  KR: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  OM: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  PK: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  PS: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  PH: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  QA: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  RU: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  SA: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  LK: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  SY: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  TW: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  TJ: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  TH: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  TL: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  TR: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  AE: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  UZ: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  VN: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+  YE: {
+    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
+    price: '25.00',
+    name: 'Express 2-4 Working Days From Dispatch',
+  },
+};
--- packages/frontend/src/state/migrators/v0.5.0/state.js
+++ packages/frontend/src/state/migrators/v0.5.0/state.js
@@ -1,41 +1,12 @@
 import prevState from '../v0.4.0/state';
-
-const DSMLRates = {
-  US: {
-    rate: 'shopify-Express%201-3%20working%20days%20from%20dispatch-20.00',
-    price: '20.00',
-    name: 'Express 1-3 Working Days From Dispatch',
-  },
-  UK: {
-    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-5.00',
-    price: '5.00',
-    name: 'Express 1-2 Working Days From Dispatch',
-  },
-  // any country that isn't the UK that's a part of the EU
-  EU: {
-    rate: 'shopify-Express%201-2%20working%20days%20from%20dispatch-15.00',
-    price: '15.00',
-    name: 'Express 1-2 Working Days From Dispatch',
-  },
-  SG: {
-    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
-    price: '25.00',
-    name: 'Express 2-4 Working Days From Dispatch',
-  },
-  // any country that isn't SG that's inside of ASIA
-  ASIA: {
-    rate: 'shopify-Express%202-4%20working%20days%20from%20dispatch-25.00',
-    price: '25.00',
-    name: 'Express 2-4 Working Days From Dispatch',
-  },
-};
+import dsmlRates from '../../../constants/dsmlRates';
 
 const updateProfile = profile => {
   if (!profile.shipping || !profile.shipping.country) {
     return profile;
   }
 
-  const rate = DSMLRates[profile.shipping.country.value];
+  const rate = dsmlRates[profile.shipping.country.value];
 
   // we're safe to push it onto the array, because even if it's empty it will be the first index
   return {
