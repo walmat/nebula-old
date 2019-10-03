2d447775156385a9b73f47a2b9ce7fbacb1ffd77
Matthew Wall <matthew.wallt@gmail.com>
2019-10-03 03:34:47 -0400

    rm log

--- packages/frontend/lib/task/adapter.js
+++ packages/frontend/lib/task/adapter.js
@@ -44,10 +44,9 @@ class TaskManagerAdapter {
 
     // TODO: Research if this should always listened to, or if we can dynamically
     //       Start/Stop listening like we with task events
-    this._taskManager._events.on(TaskManager.Events.StartHarvest, (...args) => {
-      console.log(args);
-      ipcRenderer.send(IPCKeys.RequestStartHarvestCaptcha, ...args);
-    });
+    this._taskManager._events.on(TaskManager.Events.StartHarvest, (...args) =>
+      ipcRenderer.send(IPCKeys.RequestStartHarvestCaptcha, ...args),
+    );
     this._taskManager._events.on(TaskManager.Events.StopHarvest, (...args) =>
       ipcRenderer.send(IPCKeys.RequestStopHarvestCaptcha, ...args),
     );
