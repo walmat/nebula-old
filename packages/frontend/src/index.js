ac3c28ea4568ffd97d647614740e57676cad9d03
Matthew Wall <matthew.wallt@gmail.com>
2019-10-28 17:41:32 -0400

    rm some unused scripts

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom';
 import createApp from './app';
-// import registerServiceWorker from './registerServiceWorker';
 import configureStore from './state/configureStore';
 import { globalActions } from './state/actions';
 
@@ -8,4 +7,3 @@ const store = configureStore();
 store.dispatch(globalActions.migrateState());
 
 ReactDOM.render(createApp(store), document.getElementById('root'));
-// registerServiceWorker();
